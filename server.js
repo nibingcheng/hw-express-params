b14f8cc7547f5353a3e754125c6cdd26886b41a2
Bingcheng Ni <nibingcheng@johndeere.com>
2021-01-25 17:59:06 -0600

    Commit 2: Tip Calculator express application created.

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const app = express();
 const {query} = require("express");
 
 // Get route for greeting
-let personName = "Jimmy-boy";
-
 app.get('/greeting/:name', (req, res)=>{
-    res.send(`Hello, ${personName}!`);
+    res.send(`What's up, ${req.params.name}!`);
 })
 
-
+// Get route for tip calc
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    let total = req.params.total;
+    let tipPercentage = req.params.tipPercentage;
+    res.send(` ${total*tipPercentage/100} `);
+})
 
 app.listen(3000, () => {
     console.log("Server is listening!!!")
